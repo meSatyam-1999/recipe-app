d80857bcfa45b8c620dcf1cb5d26568df1ce2b98
Satyam Singh <mesatyamsingh1999@gmail.com>
2024-09-16 17:35:07 +0530

    api setup

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,39 +1,45 @@
-import { Heart, HeartPulse, Soup } from "lucide-react"
+import { Heart, HeartPulse, Soup } from "lucide-react";
 
-
-const RecipeCard = () => {
+const RecipeCard = (recipe) => {
   return (
     <div className="flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden relative p-3">
-        <a href="#" className="relative h-32">
-            <img className="rounded-md w-full h-full object-cover cursor-pointer" src="/first.jpg" alt="first card" />
-            <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm">
-                <Soup size={16} />4 servings
-            </div>
-
-            <div className="absolute top-1 right-2 bg-white rounded-full cursor-pointer p-1">
-                <Heart size={20} className="hover:fill-red-500 hover:text-red-500" />
-            </div>
-        </a>
-
-        <div className="flex mt-1">
-            <p className="font-bold tracking-wide">Chicken Kabab</p>
-        </div>
-        <p className="my-2">Hyderabadi Kabab</p>
-        <div className="flex mt-auto gap-2">
-            <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
-                <HeartPulse size={16} />
-                <span className="text-sm tracking-tighter font-semibold">Oil free</span>
-            </div>
-           
-            <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
-                <HeartPulse size={16} />
-                <span className="text-sm tracking-tighter font-semibold">Best Kabab</span>
-            </div>
-
+      <a href="#" className="relative h-32">
+        <img
+          className="rounded-md w-full h-full object-cover cursor-pointer"
+          src={recipe.image}
+          alt="first card"
+        />
+        <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm">
+          <Soup size={16} />
+          {recipe.yield} servings
         </div>
 
+        <div className="absolute top-1 right-2 bg-white rounded-full cursor-pointer p-1">
+          <Heart size={20} className="hover:fill-red-500 hover:text-red-500" />
+        </div>
+      </a>
+
+      <div className="flex mt-1">
+        <p className="font-bold tracking-wide">{recipe.label}</p>
+      </div>
+      <p className="my-2">{recipe.cuisineType}</p>
+      <div className="flex mt-auto gap-2">
+        <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
+          <HeartPulse size={16} />
+          <span className="text-sm tracking-tighter font-semibold">
+            Oil free
+          </span>
+        </div>
+
+        <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
+          <HeartPulse size={16} />
+          <span className="text-sm tracking-tighter font-semibold">
+            Best Kabab
+          </span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecipeCard
+export default RecipeCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,42 +1,78 @@
-import { Search } from "lucide-react"
-import RecipeCard from "../components/RecipeCard"
+import { Search } from "lucide-react";
+import RecipeCard from "../components/RecipeCard";
+import { useEffect, useState } from "react";
 
+const API_ID = "59640b7d";
+const API_KEY = "63daab30c9a076aa68f67ef6227ea524";
 
 const HomePage = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchRecipes = async (searchQuery) => {
+    setLoading(true);
+    setRecipes([]);
+
+    try {
+      const res = await fetch(
+        `https://api.edamam.com/api/recipes/v2/?app_id=${API_ID}&app_key=${API_KEY}&q=${searchQuery}&type=public`
+      );
+      const data = await res.json();
+      setRecipes(data.hits);
+      console.log(data.hits);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecipes("chicken");
+  }, []);
+
   return (
     <div className="bg-[#faf9fb] p-10 flex-1">
-        <div className="max-w-screen-lg mx-auto">
-        
+      <div className="max-w-screen-lg mx-auto">
         <form>
           <label className="input shadow-md flex items-center gap-2">
             <Search size={24} />
-            <input type="text" placeholder="Search for a recipe..." className="flex-1" />
+            <input
+              type="text"
+              placeholder="Search for a recipe..."
+              className="flex-1"
+            />
           </label>
         </form>
 
         <h1 className="font-bold text-3xl md:text-5xl mt-4">
-            Recommended Recipies
+          Recommended Recipies
         </h1>
 
         <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">
-            Popular Choices
+          Popular Choices
         </p>
 
-      <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-      </div>
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
 
+        {!loading && recipes.map(({recipe},index) => (
+          <RecipeCard key={index} recipe={recipe} />
+        ))}
+
+
+          {loading &&
+            [...Array(9)].map((_, index) => (
+              <div key={index} className="flex w-full flex-col gap-4">
+                <div className="skeleton h-32 w-full"></div>
+                <div className="flex justify-between"></div>
+                <div className="skeleton h-4 w-28"></div>
+                <div className="skeleton h-4 w-24"></div>
+              </div>
+            ))}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
